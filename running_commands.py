e1df3a5f85dfa39f7bc04673f81f438fb331df3b
Nsindiso Mnqobi Matshanga <nsindiso.matshanga@econet.co.zw>
2021-08-15 11:19:06 +0200

    Sending_text_to_files

--- running_commands.py
+++ running_commands.py
@@ -1,10 +1,19 @@
 import os
+from datetime import date
 
-ip = raw_input("Please type the subnet you want to investigate ")
-
+today= date.today()
+Date = str(today.strftime("%d_%m_%y"))
+ip = input("Please type the subnet you want to investigate? ")
+with open("Shodan_logs_"+Date+".txt", "w+") as file:
+    print("File has been created")
+    
 host = 0
-for host in range(1,255,1):
-    cmd = 'shodan host  '  +ip + "."  + str(host)
-    print("......................" + cmd + "....................")
+
+for host in range(1,3,1):
+
+    cmd = 'ping -c2 '  + ip + "."  + str(host) + " >> "+ "Shodan_logs_" + Date +".txt"
+    print("......................" + ip +"."+ str(host) + "....................")
     os.system(cmd)
-    print("......................" + "Done" + "....................")
+    print("......................" + "Done"+ "....................")
+    
+        
